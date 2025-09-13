e2f30481cbfe1a59d69fd1dca5590690d1d42012
vmqxdv <vquintilio@gmail.com>
2025-09-13 11:41:54 +0200

    first commit

---
+++ src/main/java/org/lessons/java/spring_intro/SpringIntroApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroApplication.class, args);
+	}
+
+}
