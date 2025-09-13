5d8b0b296143ed1c030c2efb9a5d45ee9f8709c0
vmqxdv <vquintilio@gmail.com>
2025-09-13 12:26:03 +0200

    feat: Aggiunta pagina greetings

--- src/main/java/org/lessons/java/spring_intro/controller/HomeController.java
+++ src/main/java/org/lessons/java/spring_intro/controller/HomeController.java
@@ -1,8 +1,10 @@
 package org.lessons.java.spring_intro.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -10,8 +12,14 @@ public class HomeController {
 
   @GetMapping("/")
   public String home() {
-
     return "home";
+  }
+
+  @GetMapping("/greetings")
+  public String greetings(@RequestParam(name = "name") String name, Model model) {
+    model.addAttribute("name", name);
+
+    return "greetings";
   }
 
 }
